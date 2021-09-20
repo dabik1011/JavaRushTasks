ea3c06f257c6ea6aec9982e9dc358561a0a2584f
dabik1011 <dabik1011@gmail.com>
2021-09-21 02:53:35 +0300

    21.09.2021

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task18.task1823;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,14 +11,50 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while (!(line = reader.readLine()).equals("exit")) {
+            new ReadThread(line).start();
+        }
+
 
     }
 
     public static class ReadThread extends Thread {
+        private String fileName;
+
         public ReadThread(String fileName) {
             //implement constructor body
+            this.fileName = fileName;
+
         }
         // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+
+
+            int max = 0;
+            int maxI = 0;
+            int[] buffer = new int[256];
+            try (FileInputStream inputStream = new FileInputStream(fileName)) {
+                while (inputStream.available() > 0) {
+                    buffer[inputStream.read()]++;
+                }
+                for (int i = 0; i < buffer.length; i++) {
+                    if (max < buffer[i]) {
+                        max = buffer[i];
+                        maxI = i;
+                    }
+                    resultMap.put(fileName, maxI);
+                }
+            } catch (FileNotFoundException exception) {
+                exception.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
